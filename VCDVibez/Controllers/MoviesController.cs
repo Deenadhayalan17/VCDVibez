bc48d9504c4d77da7a55abde5e04ebf094abc754
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-06-23 00:18:28 +0530

    Basic MVC Action calls

---
+++ VCDVibez/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VCDVibez.Models;
+
+namespace VCDVibez.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name="Iron Man"} ;
+            
+            return View(movie);
+        }
+    }
+}
