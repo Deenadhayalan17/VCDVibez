8f4cd9855f9096c30c7407ff1479c7230daee74c
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-06-23 23:33:50 +0530

    Added Edit , Index action Method

--- VCDVibez/Controllers/MoviesController.cs
+++ VCDVibez/Controllers/MoviesController.cs
@@ -13,8 +13,29 @@ namespace VCDVibez.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name="Iron Man"} ;
-            
+
             return View(movie);
+            //return Content("Hi da");
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = '1', sort="mass" });
+        }
+        public ActionResult Edit(int id)
+
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int ?pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
         }
     }
 }
